d8164d94378c529f62d593ba66bcf0adfc5af9f4
FuryForev3r <messiasfuryforever@gmail.com>
2024-07-30 11:22:21 -0300

    Animated Sprite Added

--- RobEngine/__init__.py
+++ RobEngine/__init__.py
@@ -1,8 +1,16 @@
 from .robengine import RobEngine
-from .sprite import Sprite
+from .sprite import Sprite, AnimatedSprite
 from .types import locals
 from .input import Input
 from .spritesgroup import SpriteGroup
 from .physics import Physics
 
-__all__ = ["RobEngine", "Sprite", "locals", "Input", "SpriteGroup", "Physics"]
+__all__ = [
+    "RobEngine",
+    "Sprite",
+    "locals",
+    "Input",
+    "SpriteGroup",
+    "Physics",
+    "AnimatedSprite",
+]
--- RobEngine/sprite.py
+++ RobEngine/sprite.py
@@ -1,5 +1,6 @@
-from pygame import image, Surface, sprite, transform
+from pygame import image, Surface, sprite, time, transform
 from .log import logger
+import time
 
 
 class Sprite(sprite.Sprite):
@@ -34,6 +35,61 @@ class Sprite(sprite.Sprite):
             logger.info(f"{self} was resized sucefully")
         except Exception as error:
             logger.error(f"ERROR: {error}")
+
+    def draw(self, screen: Surface):
+        """Draw the sprite on the screen."""
+        try:
+            screen.blit(self.image, self.rect)
+            logger.info(f"{self} - Drawn sprite.")
+        except Exception as error:
+            logger.error(f"ERROR: {error}")
+
+
+class AnimatedSprite(sprite.Sprite):
+    """A sprite that can animate."""
+
+    def __init__(
+        self, image_paths: list[str], position: list[int], animation_speed: float = 0.1
+    ):
+        self.frames = [image.load(path) for path in image_paths]
+        self.current_frame = 0
+        self.animation_speed = animation_speed
+        self.last_update_time = time.time()
+        self.image = self.frames[self.current_frame]
+        self.rect = self.image.get_rect()
+        self.rect.left = position[0]
+        self.rect.top = position[1]
+
+        logger.info(f"{self} - Sprite animation loaded.")
+        logger.info(f"{self} - Sprite rect defined")
+        logger.info(f"{self} - Position set to {position}")
+
+    def set_position(self, x: int, y: int):
+        """Update position of the sprite."""
+        try:
+            self.rect.x = x
+            self.rect.y = y
+            logger.info(f"{self} - Position set to [{y, x}]")
+
+        except Exception as error:
+            logger.error(f"ERROR: {error}")
+
+    def resize(self, size):
+        try:
+            self.image = transform.scale(self.image, size)
+            self.rect = self.image.get_rect(topleft=self.rect.topleft)
+
+            logger.info(f"{self} was resized successfully")
+        except Exception as error:
+            logger.error(f"ERROR: {error}")
+
+    def update(self):
+        """Update the animation frame."""
+        now = time.time()
+        if now - self.last_update_time > self.animation_speed:
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.image = self.frames[self.current_frame]
+            self.last_update_time = now
 
     def draw(self, screen: Surface):
         """Draw the sprite on the screen."""
